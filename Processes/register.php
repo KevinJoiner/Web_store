f4da8494736b1ae02310a885488d9d7d6aed5ba6
KevinJoiner <Raynardjoiner@gmail.com>
2016-04-14 19:24:59 -0400

    Changed mysqli to oop

--- Processes/register.php
+++ Processes/register.php
@@ -5,7 +5,7 @@
  * Date: 4/3/2016
  * Time: 6:44 PM
  */
-
+require_once('../../sql_connector.php');
 
 if(isset($_POST['submit'])) {
 
@@ -14,59 +14,55 @@ if(isset($_POST['submit'])) {
     if (empty($_POST['name'])) {
         $data_missing[] = 'name';}
     else{
-        $f_name = $_POST['name'];
+        $f_name = $mysqli->real_escape_string($_POST['name']);
 
-        }
+    }
 
 
     if (empty($_POST['email'])) {
         $data_missing[] = 'email';}
     else{
-        $f_email = $_POST['email'];
+        $f_email = $mysqli->real_escape_string($_POST['email']);
     }
 
 
     if (empty($_POST['password'])) {
         $data_missing[] = 'password';}
     else{
-        $f_password = $_POST['password'];
+        $f_password = $mysqli->real_escape_string($_POST['password']);
     }
 
 
     if (empty($_POST['streetaddress'])) {
         $data_missing[] = 'streetaddress';}
     else{
-        $f_streetaddress = $_POST['streetaddress'];
+        $f_streetaddress = $mysqli->real_escape_string($_POST['streetaddress']);
     }
 
 
     if (empty($_POST['cityaddress'])) {
         $data_missing[] = 'cityaddress';}
     else{
-        $f_cityaddress = $_POST['cityaddress'];
+        $f_cityaddress = $mysqli->real_escape_string($_POST['cityaddress']);
     }
 
 
     if(empty($data_missing)){
-        require_once('../../sql_connector.php');
         $query = "Insert INTO customer (Name,password,streetaddress,cityaddress,email) VALUES (?,?,?,?,?)";
-        $stmt = mysqli_prepare($dbc,$query);
+        $stmt = $mysqli->prepare($query);
 
-        mysqli_stmt_bind_param($stmt,"sssss",$f_name,$f_password,$f_streetaddress,$f_cityaddress,$f_email);
-        mysqli_stmt_execute($stmt);
-        $affected_rows = mysqli_stmt_affected_rows($stmt);
+        $stmt->bind_param("sssss",$f_name,$f_password,$f_streetaddress,$f_cityaddress,$f_email);
+        $stmt->execute();
+        //$affected_rows = $mysqli->rowCount();
 
-        if($affected_rows == 1){
-            echo 'You are all signed up';
-        }
-        else{
-            echo 'You must have entered something wrong go back and try again';
-            echo "Don't forget that you can only use and email address once";
+        if($mysqli->affected_rows == 1){
+            echo 'Student Entered';
         }
 
-	            mysqli_stmt_close($stmt);
 
-            mysqli_close($dbc);
+        $stmt->close();
+
+        $mysqli->close();
 
     }
 
